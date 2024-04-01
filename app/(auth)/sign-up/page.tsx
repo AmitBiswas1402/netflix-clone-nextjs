d3570337bd7d997ec8ba0a1c10480d94db520c10
Amit Biswas <amit.142biswas@gmail.com>
2024-04-01 14:49:17 +0530

    tailwind sucking

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,10 @@
-
-export default function Login() {
-    return (
-      <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
-        signup
-      </div>
-    )
-  }
-  
-  
+export default function SignUp() {
+  return (
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form>
+        <h1 className="text-3xl font-semibold text-white">Signup</h1>
+        <div></div>
+      </form>
+    </div>
+  );
+}
